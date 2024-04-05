f3a5053f6f593a4d275d4218a57bd4c384a3dc55
JoseGonzCSE <jgonzalez437@ucmerced.edu>
2024-04-04 20:58:36 -0400

    Cleaned up

--- PASScript.py
+++ PASScript.py
@@ -3,41 +3,28 @@ import string #Gives us our aplha-numerical-special characters
 import pyautogui # keybord/mouse
 import time # for pauses
 
-#Use to get mouse location to click where you need to go
-#print(pyautogui.position())
-
-
-#change email as necessary
+#Create Account Information
 email="j.andresgonzalez1010@gmail.com"
-passwordLength=12
-
+passwordLength=17
 passwordLetters=string.ascii_letters
 passwordDigits=string.digits
 passwordPunctuation=string.punctuation
-#Change inclusion of Puncuation as necessary
 password=''.join(random.choices(passwordLetters+passwordDigits+passwordPunctuation,k=passwordLength))
 
+#Script
 
 #Start delay
 time.sleep(1) 
-
-webBrowser=pyautogui.click(1886,381)
-webBrowser
-
+#Use to get mouse location to click where you need to go
+#print(pyautogui.position())
+webBrowser=pyautogui.click(1886,381) #My task bar is on the right of the screen, vertical
 emailLocation=pyautogui.click(813,592)
-emailLocation
 pyautogui.typewrite(email)
 #time.sleep(1)
-
-
 passwordLocation=pyautogui.click(687,699)
-passwordLocation
 pyautogui.typewrite(password)
 #time.sleep(1)
-
-
 confirmPasswordLocation=pyautogui.click(688,812)
-confirmPasswordLocation
 pyautogui.typewrite(password)
 time.sleep(1)
 createAccount=pyautogui.click(696,891)
